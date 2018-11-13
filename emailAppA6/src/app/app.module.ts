ef5969a78014b289ac325efeb393cfe7398809db
sbrycebarker <sbrycesti@gmail.com>
2018-11-13 16:02:46 -0700

    con

--- emailAppA6/src/app/app.module.ts
+++ emailAppA6/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EmailformComponent } from './emailform/emailform.component';
 
@@ -10,7 +10,8 @@ import { EmailformComponent } from './emailform/emailform.component';
     EmailformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
